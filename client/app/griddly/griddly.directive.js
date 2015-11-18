2991d53218445502f816c113ea43cb6234cf8924
lindvi <charlie.lindviken@valtech.se>
2015-11-18 19:28:27 +0100

    Added sleep mode to light controller

--- client/app/griddly/griddly.directive.js
+++ client/app/griddly/griddly.directive.js
@@ -1,10 +1,11 @@
 'use strict';
 
-app.directive('griddly', function () {
+app.directive('griddly', ['$http', function ($http) {
   return {
     templateUrl: 'app/griddly/griddly.html',
     restrict: 'EA',
     link: function ($scope, element, attrs) {
+      $scope.pictures = [];
       $scope.grid = {
         width: 2,
         height: 2,
@@ -18,13 +19,24 @@ app.directive('griddly', function () {
         }
       };
 
+      $http({
+        method: 'GET',
+        url: 'https://www.reddit.com/r/earthporn.json'
+      }).success(function(promise) {
+        $scope.pictures = promise.data.children;
+        setBackground(Math.round(Math.random()*$scope.pictures.length));
+      });
+
+
+
       $scope.grid.add({
         width: 1,
         height: 2,
         x: 0,
         y: 0,
         color: '#'+Math.floor(Math.random()*16777215).toString(16),
-        view: 'app/views/_clock.html'
+        view: 'app/views/_clock.html',
+        blur: true
       });
 
       $scope.grid.add({
@@ -33,15 +45,31 @@ app.directive('griddly', function () {
         x: 1,
         y: 0,
         color: '#'+Math.floor(Math.random()*16777215).toString(16),
-        view: 'app/views/_lights.html'
+        view: 'app/views/_lights.html',
+        blur: false
       });
       $scope.grid.add({
         width: 1,
         height: 1,
         x: 1,
         y: 1,
-        color: '#'+Math.floor(Math.random()*16777215).toString(16)
+        color: '#'+Math.floor(Math.random()*16777215).toString(16),
+        blur: false
       });
+
+
+
+      function setBackground(index) {
+        $scope.background = $scope.pictures[index];
+        if($scope.background.data.domain === 'imgur.com'){
+          $scope.background.data.url = $scope.background.data.url +'.jpg';
+        }
+
+        angular.element('.bgImg').css({
+          'background-image': 'url(' + $scope.background.data.url +')',
+          'background-size' : 'cover'
+        });
+      }
     }
   };
-});
+}]);
