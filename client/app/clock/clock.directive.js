f6985077cd9d1d26f707b1c4866b81816f621cdc
lindvi <charlie.lindviken@valtech.se>
2015-11-23 21:39:44 +0100

    Updated some styling and fixed metro lines

--- client/app/clock/clock.directive.js
+++ client/app/clock/clock.directive.js
@@ -4,6 +4,7 @@ app.directive('clock', ['$interval', function ($interval) {
   return {
     templateUrl: 'app/clock/clock.html',
     restrict: 'E',
+    replace: true,
     link: function ($scope, element, attrs) {
      var tick = function() {
       $scope.clock = Date.now();
--- client/app/traffic/traffic.directive.js
+++ client/app/traffic/traffic.directive.js
@@ -5,15 +5,24 @@ app.directive('traffic', ['TrafficFactory', function (TrafficFactory) {
     templateUrl: 'app/traffic/traffic.html',
     restrict: 'EA',
     link: function ($scope, element, attrs) {
+      $scope.currentStation = {};
+      $scope.currentStation.Name = 'Gullmarsplan';
 
-      $scope.currentStation = false;
 
-      TrafficFactory.stations({'station': 'Gullmarsplan', 'stationsOnly': true, 'maxResults': 5}).success(function(promise){
-        $scope.stations = promise.ResponseData;
-      });
 
+      $scope.searchForStation = function() {
+        TrafficFactory.stations({'station': $scope.currentStation.Name, 'stationsOnly': true, 'maxResults': 5}).success(function(promise){
+          $scope.stations = promise.ResponseData;
+          $scope.checkTimetable($scope.stations[0]);
+
+        });
+      };
 
       $scope.checkTimetable = function(station) {
+        if(station === undefined) {
+          return;
+        }
+
         $scope.currentStation = station;
         TrafficFactory.timetableForStation(station.SiteId)
         .success(function(promise){
@@ -24,6 +33,8 @@ app.directive('traffic', ['TrafficFactory', function (TrafficFactory) {
       $scope.clearStation = function() {
         $scope.currentStation = false;
       }
+
+      $scope.searchForStation();
     }
   };
 }]);
