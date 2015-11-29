21e56542821081b4da197a43b00aab6740d959ec
lindvi <charlie.lindviken@valtech.se>
2015-11-29 15:00:28 +0100

    Commit new assets

---
+++ client/app/background/background.service.js
@@ -0,0 +1,35 @@
+'use strict';
+/* global app:false */
+
+app.factory('BackgroundFactory', ['$http', function ($http) {
+  var backgroundFactory = {};
+  backgroundFactory.images = [];
+  backgroundFactory.current = {};
+
+  backgroundFactory.fetchImages = function() {
+    return $http({
+      method: 'GET',
+      url: 'https://www.reddit.com/r/earthporn.json'
+    }).success(function(promise) {
+      backgroundFactory.images = promise.data.children;
+    });
+  };
+
+  backgroundFactory.randomImage = function() {
+    backgroundFactory.setImage(Math.round(Math.random()*backgroundFactory.images.length));
+  };
+
+  backgroundFactory.setImage = function(index) {
+    backgroundFactory.current = backgroundFactory.images[index];
+    if(backgroundFactory.current.data.domain === 'imgur.com'){
+      backgroundFactory.current.data.url = backgroundFactory.current.data.url +'.jpg';
+    }
+
+    angular.element('.bgImg').css({
+      'background-image': 'url(' + backgroundFactory.current.data.url +')',
+      'background-size' : 'cover'
+    });
+  };
+
+  return backgroundFactory;
+}]);
---
+++ client/app/background/background.service.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Service: background', function () {
+
+  // load the service's module
+  beforeEach(module('charaApp'));
+
+  // instantiate service
+  var background;
+  beforeEach(inject(function (_background_) {
+    background = _background_;
+  }));
+
+
+});
